1b244f225a3703275d6208ae0da26e9195ae1098
biwst1 <60522773+biwst1@users.noreply.github.com>
2021-02-11 14:29:32 -0600

    Add ChallengeThree project

---
+++ ChallengeThree/CalculateItemSummary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeThree
+{
+    public class CalculateItemSummary
+    {
+        public ItemSummary[] GetSubItemSummary(string itemNumber, List<Item> allItems)
+        {
+            Item[] items = GetSubItems(itemNumber, allItems);
+            List<ItemSummary> summaries = new List<ItemSummary>();
+            foreach(Item item in items)
+            {
+                summaries.Add(ItemSummary.CreateItemSummary(item));
+            }
+
+            return summaries.ToArray();
+        }
+
+        /// <summary>
+        /// Use recursive way to loop each item's subitems for the item which has matched itemNumber.
+        /// </summary>
+        /// <param name="itemNumber"></param>
+        /// <param name="allItems"></param>
+        /// <returns></returns>
+        private Item[] GetSubItems(string itemNumber, List<Item> allItems)
+        {
+            List<Item> items = new List<Item>();
+            foreach(Item item in allItems)
+            {
+                if (item.ItemNumber == itemNumber)
+                {
+                    items.Add(item);
+                    Item[] result = GetSubItems(itemNumber, item.SubItems);
+                    items.AddRange(result);
+                }
+            }
+
+            return items.ToArray();
+        }
+    }
+}
