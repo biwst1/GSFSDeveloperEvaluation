56e496e580f1ee8b8ca48e241fa9189cfa7f0aac
biwst1 <60522773+biwst1@users.noreply.github.com>
2021-02-11 14:30:29 -0600

    Add ChallengeFour project

---
+++ ChallengeFour/parentheses.js
@@ -0,0 +1,30 @@
+﻿let isParenthesisClosed = function (parenthesis) {
+    let stack = [];
+    let openCloseMap = {
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+
+    if (parenthesis.length % 2 != 0) {
+        return false;
+    }
+
+    for (let i = 0; i < parenthesis.length; i++) {
+        if (parenthesis[i] == '(' || parenthesis[i] == '[' || parenthesis[i] == '{') {
+            stack.push(parenthesis[i]);
+        }
+        else {
+            let open = stack.pop();
+            if (parenthesis[i] != openCloseMap[open]) {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length != 0) {
+        return false;
+    }
+
+    return true;
+}
