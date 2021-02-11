e2478497518573f8b836e095c9f95ef66d53df20
biwst1 <60522773+biwst1@users.noreply.github.com>
2021-02-11 14:28:29 -0600

    Add ChallengeOne project

---
+++ ChallengeOne/ICalculateOhmValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChallengeOne
+{
+    public interface ICalculateOhmValues
+    {
+        /// <summary>
+        /// Calculates the Ohm value of a resistor based on the band colors.
+        /// </summary>
+        /// <param name="bandColors">array of band colors</param>
+        /// <returns>ohm value in the string format as "resistance tolerance temperature-coefficient"</returns>
+        string CalculateOhmValue(string[] bandColors);
+    }
+}
